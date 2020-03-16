6d308357256fdeb9137672ea77a694830f591c83
Cal Donovan <caldonovan@outlook.com>
2020-03-16 17:21:33 +0000

    Add Project controller with views.

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Projects</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in and this is projects page!
+
+                    <ul>
+                        @foreach ($projects as $project)
+                            <li>{{ $project->project_name }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
