6118caf81e29afdd0509d1157da7a0fd420d55fc
Cal Donovan <caldonovan@outlook.com>
2020-03-17 15:07:04 +0000

    Nav update.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,24 @@
             </div>
 
             <ul class="list-unstyled components">
-                <p>User information goes here</p>
+                <p>User information goes here (eventually)</p>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        User Name<span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
                 <li class="active">
                     <a href="#">Home</a>
                 </li>
