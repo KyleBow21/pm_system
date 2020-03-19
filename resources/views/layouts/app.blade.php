134fc8b7cb8c51e19dd2e80f8e4147b2fb30be16
caldonovan <caldonovan@outlook.com>
2020-03-19 12:56:57 +0000

    Sync for other PC.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,9 @@
                     <a href="#">Assignments</a>
                 </li>
                 <li>
+                    <a href="#">Projects</a>
+                </li>
+                <li>
                     <a href="#">Marks</a>
                 </li>
             </ul>
