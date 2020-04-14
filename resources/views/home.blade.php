a6b6acb03f30c7635993db81a65a7ed0fcebe4b6
Cal Donovan <caldonovan@outlook.com>
2020-04-14 17:39:47 +0100

    Updated dashboard layout.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,9 @@
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-10" id="dashboard">
+            <div class="card-deck">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Modules</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -20,14 +21,88 @@
 
                     <!-- Show if the user has logged in -->
                     @auth
-                    <h5>Modules</h5><hr>
-
                     @foreach ($modules as $module)
                         <li>{{ $module->module_name }} <i data-feather="chevron-right"></i></li>
                     @endforeach
                     @endauth
                 </div>
             </div>
+
+            <div class="card">
+                <div class="card-header">Assignments</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <!-- Show if the user has no logged in -->
+                    @guest
+                        <h4>You must log-in to use this service!</h4>
+                    @endguest
+
+                    <!-- Show if the user has logged in -->
+                    @auth
+                    @foreach ($modules as $module)
+                        <li>{{ $module->module_name }} <i data-feather="chevron-right"></i></li>
+                    @endforeach
+                    @endauth
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center pt-5">
+        <div class="col-md-10" id="dashboard">
+            <div class="card-deck">
+            <div class="card">
+                <div class="card-header">Projects</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <!-- Show if the user has no logged in -->
+                    @guest
+                        <h4>You must log-in to use this service!</h4>
+                    @endguest
+
+                    <!-- Show if the user has logged in -->
+                    @auth
+                    @foreach ($modules as $module)
+                        <li>{{ $module->module_name }} <i data-feather="chevron-right"></i></li>
+                    @endforeach
+                    @endauth
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">Marks</div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <!-- Show if the user has no logged in -->
+                    @guest
+                        <h4>You must log-in to use this service!</h4>
+                    @endguest
+
+                    <!-- Show if the user has logged in -->
+                    @auth
+                    @foreach ($modules as $module)
+                        <li>{{ $module->module_name }} <i data-feather="chevron-right"></i></li>
+                    @endforeach
+                    @endauth
+                </div>
+            </div>
+        </div>
         </div>
     </div>
 </div>
