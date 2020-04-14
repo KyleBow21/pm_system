3ccc0678298f1d499fa3ccc58f9dc524fdb59581
Kyle Jenkins <kylej821@yahoo.com>
2020-04-14 16:18:39 +0100

    Fix User model

--- app/User.php
+++ app/User.php
@@ -6,16 +6,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-<<<<<<< HEAD
 
 
-
-
-
-
-
-=======
->>>>>>> 29a21b3d6aa01b1dcaeede2c327254342da9e01d
 class User extends Authenticatable
 {
     use Notifiable;
@@ -46,19 +38,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function project()
-   {
-     return $this->belongsTo('App\Project');
-   }
-
-   public function scheme()
-   {
-     return $this->belongsTo('App\Scheme');
-   }
-
-   public function modules()
-   {
-     return $this->belongsToMany('App\Module');
-   }
 }
