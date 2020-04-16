d63c19a44a5db0bfc1d1e3be37de314262c94b77
tempname0606 <879228@swansea.ac.uk>
2020-04-16 08:18:59 +0100

    Fixed password seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $u = new User();
+        $u->name = "Ben Mora";
+        $u->email = "mora@gmail.com";
+        $u->role = "Admin";
+        $u->scheme_id = 1;
+        $u->module_id = 1;
+        $u->password = bcrypt('password');
+        $u->save();
+    }
+}
